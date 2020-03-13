194c5d263cc861f0a29c532bd0bb0f1a0137babb
bwuning <bwuning@outlook.com>
2020-03-13 14:23:06 +0800

    edit dependencies

--- packages/show-rn-code/rollup.config.js
+++ packages/show-rn-code/rollup.config.js
@@ -1,7 +1,6 @@
 // rollup.config.js
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss'
-import { uglify } from "rollup-plugin-uglify";
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
@@ -13,21 +12,13 @@ export default {
         }),
         typescript({
             tsconfig: "./tsconfig.json",
-        }),
-        // uglify({
-        //     sourcemap: true
-        // }),
+        })
     ],
     output: [
         {
             file: 'dist/bundle.cjs.js',
             format: 'cjs',
             sourcemap: true
-        },
-        // {
-        //     file: 'dist/bundle.esm.js',
-        //     format: 'esm',
-        //     sourcemap: true
-        // }
+        }
     ]
 };
